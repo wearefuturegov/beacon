0f335960eb2f0832475abdd07c01b939d0c3f37d
Jamie <jbuckley760@gmail.com>
2020-03-28 14:00:49 +0000

    Add index to user.email column

--- db/migrate/20200328103438_add_attributes_to_users.rb
+++ db/migrate/20200328103438_add_attributes_to_users.rb
@@ -1,6 +1,7 @@
 class AddAttributesToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :email, :text, null: false
+    add_index :users, :email
     add_column :users, :invited, :datetime, null: false
     add_column :users, :admin, :boolean, null: false, default: false
     add_column :users, :last_logged_in, :datetime
