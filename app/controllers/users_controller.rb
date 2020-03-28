62689c057a9f6d3c2e3721b99cb7bd6438f69309
Jamie <jbuckley760@gmail.com>
2020-03-28 13:53:35 +0000

    Initial work on email functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:destroy]
+
   # GET /users
   def index
     @users = User.all
@@ -21,8 +23,20 @@ class UsersController < ApplicationController
     end
   end
 
-  # Only allow a list of trusted parameters through.
-  def user_params
-    params.require(:user).permit(:email, :admin)
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully destroyed.'
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:email, :admin)
+    end
 end
