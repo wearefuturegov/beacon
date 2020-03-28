7184c87030c6aacd4d8245d2e58ce6e20218a2fb
Alexander Popov <alex.val.popov@gmail.com>
2020-03-28 22:46:43 +0200

    Add Contacts#index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts
+  resources :contacts, only: [:index]
   resources :users, only: [:index, :new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'contacts#index'
