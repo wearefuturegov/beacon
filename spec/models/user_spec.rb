b14ee4c430c221a084c4b90346230d7f47ea2ab8
Jamie <jbuckley760@gmail.com>
2020-03-28 14:01:12 +0000

    Update spec to reflect optional organisation id, required email and removed validations on name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
-    it { is_expected.to belong_to(:organisation) }
+    it { is_expected.to belong_to(:organisation).optional(true) }
     it { is_expected.to have_many(:contact_list_users) }
     it { is_expected.to have_many(:contact_lists).through(:contact_list_users) }
     it { is_expected.to have_many(:contacts).through(:contact_lists) }
@@ -12,10 +12,5 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_many(:completed_tasks).conditions('completed_on IS NOT NULL') }
     it { is_expected.to have_many(:uncompleted_contacts).through(:uncompleted_tasks).source(:contact) }
     it { is_expected.to have_many(:completed_contacts).through(:completed_tasks).source(:contact) }
-  end
-
-  describe 'validations' do
-    it { is_expected.to validate_presence_of(:first_name) }
-    it { is_expected.to validate_presence_of(:last_name) }
   end
 end
