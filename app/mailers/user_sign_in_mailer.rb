5fa8ec2665bce9049a04d875f85bbd6aa7c0b360
Jamie <jbuckley760@gmail.com>
2020-03-28 16:34:14 +0000

    Add spec for mailer.  Fix typo in email body

--- app/mailers/user_sign_in_mailer.rb
+++ app/mailers/user_sign_in_mailer.rb
@@ -2,6 +2,6 @@ class UserSignInMailer < ApplicationMailer
   default :from => 'noreply@https://i-have-i-need.herokuapp.com'
 
   def send_invite_email(user)
-    mail(:to => user.email, :subject => "You've have been invited")
+    mail(:to => user.email, :subject => "You've been invited")
   end
 end
--- spec/mailers/user_sign_in_mailer_spec.rb
+++ spec/mailers/user_sign_in_mailer_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe UserSignInMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'send invite email' do
+    let(:user) { double User, email: 'test@emailaddress.com' }
+    let(:mail) { described_class.send_invite_email(user).deliver }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq("You've been invited")
+    end
+
+    it 'sends the email to the user' do
+      expect(mail.to).to eq(["test@emailaddress.com"])
+    end
+
+    it 'is sent from the applications noreply address' do
+      expect(mail.from).to eq("noreply@https://i-have-i-need.herokuapp.com")
+    end
+  end
 end
