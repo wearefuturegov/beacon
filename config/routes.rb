a99ef5b5666acd4ad60786122e702e5faab7bb32
Jamie <jbuckley760@gmail.com>
2020-03-28 13:07:12 +0000

    Update schema and re-add missing routes entry

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :contacts
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'contacts#index'
 end
