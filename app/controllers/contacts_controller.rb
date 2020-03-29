6075050e6805593fc3dbaa6e5d6c2165263fa2c0
jamie <jbuckley760@gmail.com>
2020-03-29 12:22:45 +0100

    First page implemented of contact edit flow, with placeholder controller functionality

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,22 @@
 class ContactsController < ApplicationController
+
+  before_action :set_contact, only: [:edit, :update]
+
   def index
     @contacts = Contact.all
   end
+
+  # GET /contacts/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /contacts/1
+  # PATCH/PUT /contacts/1.json
+  def update
+  end
+
+  private
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
 end
