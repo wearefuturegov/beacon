b2309bd50f4a6a4180800e45463f22dd2a4838a3
Eddie Leeper <eddie@notbinary.co.uk>
2020-03-29 13:48:37 +0100

    Implemented notes for contacts.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,34 @@
 class ContactsController < ApplicationController
+
+  before_action :set_contact, only: [:edit, :update, :show]
+
   def index
     @contacts = Contact.all
+  end
+
+  def show
+  end
+
+  # GET /contacts/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /contacts/1
+  # PATCH/PUT /contacts/1.json
+  def update
+    if @contact.update(contact_params)
+      redirect_to contacts_path, notice: 'Contact was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :middle_names, :surname, :address, :postcode, :email, :telephone, :mobile)
   end
 end
---
+++ spec/requests/notes_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Notes", type: :request do
+
+end
