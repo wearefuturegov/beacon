b6e5f841a6ca1a1bf546adeb4d069f8753b6a91a
timwis <tim@timwis.com>
2020-03-29 12:54:28 +0100

    Fix indentation of application_controller private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def current_user
-    @current_user ||= authenticate_by_session(User)
-  end
+    def current_user
+      @current_user ||= authenticate_by_session(User)
+    end
 
-  def require_user!
-    return if current_user
-    save_passwordless_redirect_location!(User)
-    redirect_to auth.sign_in_path, flash: { error: 'Please sign in' }
-  end
+    def require_user!
+      return if current_user
+      save_passwordless_redirect_location!(User)
+      redirect_to auth.sign_in_path, flash: { error: 'Please sign in' }
+    end
 end
