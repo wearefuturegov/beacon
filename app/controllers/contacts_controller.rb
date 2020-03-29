0661f3315da73549071670a1d9631c73706d4173
jamie <jbuckley760@gmail.com>
2020-03-29 13:16:27 +0100

    Add email field to contacts, and allow submitting contact changes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,10 +13,19 @@ class ContactsController < ApplicationController
   # PATCH/PUT /contacts/1
   # PATCH/PUT /contacts/1.json
   def update
+    if @contact.update(contact_params)
+      redirect_to contacts_path, notice: 'Contact was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   private
     def set_contact
       @contact = Contact.find(params[:id])
     end
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :middle_names, :surname, :address, :postcode, :email, :telephone, :mobile)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_201840) do
+ActiveRecord::Schema.define(version: 2020_03_29_120343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_201840) do
     t.integer "tasks_count", default: 0
     t.integer "uncompleted_tasks_count", default: 0
     t.integer "completed_tasks_count", default: 0
+    t.text "email"
     t.index ["contact_list_id"], name: "index_contacts_on_contact_list_id"
     t.index ["priority"], name: "index_contacts_on_priority"
   end
