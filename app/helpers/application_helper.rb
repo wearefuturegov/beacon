43cd0c2267197011d2f4f003b70039d828cb0107
jamie <jbuckley760@gmail.com>
2020-03-31 11:52:56 +0100

    Move task enums into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def format_datetime(datetime)
     datetime.strftime("%d/%m/%y %k:%M")
   end
+
+  def task_statuses
+    ['To do', 'Completed']
+  end
+
+  def task_priorities
+    %w(High Medium Low)
+  end
+
+  def task_categories
+    %w(Food Medicine Other)
+  end
 end
