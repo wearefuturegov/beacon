385f7fa66a145ff8f73027b5df3cb99329ffbee3
jamie <jbuckley760@gmail.com>
2020-03-31 11:57:34 +0100

    Move helper methods from application_helper into tasks_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,4 @@ module ApplicationHelper
   def format_datetime(datetime)
     datetime.strftime("%d/%m/%y %k:%M")
   end
-
-  def task_statuses
-    ['To do', 'Completed']
-  end
-
-  def task_priorities
-    %w(High Medium Low)
-  end
-
-  def task_categories
-    %w(Food Medicine Other)
-  end
 end
