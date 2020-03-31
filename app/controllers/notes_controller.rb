6dd38545c3841c95ffdf19eed95612afd3e1ebf2
Eddie Leeper <eddie@notbinary.co.uk>
2020-03-31 14:54:21 +0100

    73-add-notes-tasks views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,8 @@
 class NotesController < ApplicationController
   def create
-    @contact = Contact.find(params[:contact_id])
-    @note = @contact.notes.create(note_params)
-    redirect_to contact_path(@contact)
+    @task = Task.find(params[:task_id])
+    @note = @task.notes.create(note_params)
+    redirect_to task_path(@task)
   end
 
   private
