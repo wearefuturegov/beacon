b1b40a95fa6b1cc3c1abe1bac5a3e11fc51db484
jamie <jbuckley760@gmail.com>
2020-03-31 10:15:29 +0100

    Basic styling fixes for datatables

--- app/javascript/packs/datatables.js
+++ app/javascript/packs/datatables.js
@@ -1,5 +1,6 @@
 const $  = require( 'jquery' );
-const dt = require('datatables.net');
+const dt = require('datatables.net-dt');
+require('datatables.net-dt/css/jquery.dataTables.css');
 
 $(document).ready( function () {
     $('.table').first().DataTable();
