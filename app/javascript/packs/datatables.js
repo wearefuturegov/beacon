9d95131cafcc050210329d9f2892234f2769d83a
jamie <jbuckley760@gmail.com>
2020-03-31 10:29:55 +0100

    Basic column search functionality

--- app/javascript/packs/datatables.js
+++ app/javascript/packs/datatables.js
@@ -3,5 +3,25 @@ const dt = require('datatables.net-dt');
 require('datatables.net-dt/css/jquery.dataTables.css');
 
 $(document).ready( function () {
-    $('.table').first().DataTable();
+    const element = $('.table').first();
+    const table = element.DataTable();
+    element.find('tfoot th').each(function(){
+       const title = $(this).text();
+       const search = $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+
+       table.columns().every(function() {
+
+       });
+        table.columns().every( function () {
+            var that = this;
+
+            $( 'input', this.footer() ).on( 'keyup change clear', function () {
+                if ( that.search() !== this.value ) {
+                    that
+                        .search( this.value )
+                        .draw();
+                }
+            } );
+        });
+    });
 });
