dcda424e3f8acbd600e7b5f77cd11049e8d38ee1
jamie <jbuckley760@gmail.com>
2020-03-31 20:09:43 +0100

    Allow submitting needs with checkboxes and description

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -10,17 +10,21 @@ class NeedsController < ApplicationController
   end
 
   def create
-    needs_params.each do |key,value|
-      if value == "true"
-        @contact.tasks.build(name: key.humanize, due_by: DateTime.now + 7.days).save
+    needs_params["needs_list"].each do |key, value|
+      if value["active"] == "true"
+        @contact.tasks.build(category: value["name"].humanize, name: value["description"], due_by: DateTime.now + 7.days).save
       end
     end
+
+    if needs_params["other_need"]
+      @contact.tasks.build(category: "Other", name: needs_params["other_need"], due_by: DateTime.now + 7.days).save
+    end
+
     redirect_to controller: :contacts, action: :show_tasks, id: @contact.id
   end
 
   private
   def needs_params
-    params.require(:needs).permit(:groceries_and_cooked_meals, :physical_and_mental_wellbeing ,:financial_support ,
-                                  :staying_social ,:prescription_pickups ,:book_drops_and_entertainment,:dog_walking, :other)
+    params.require(:needs).permit!
   end
 end
--- app/models/needs.rb
+++ app/models/needs.rb
@@ -1,12 +1,6 @@
 class Needs
   include ActiveModel::Model
 
-  attr_accessor :groceries_and_cooked_meals
-  attr_accessor :physical_and_mental_wellbeing
-  attr_accessor :financial_support
-  attr_accessor :staying_social
-  attr_accessor :prescription_pickups
-  attr_accessor :book_drops_and_entertainment
-  attr_accessor :dog_walking
-  attr_accessor :other
+  attr_accessor :needs_list
+  attr_accessor :other_need
 end
