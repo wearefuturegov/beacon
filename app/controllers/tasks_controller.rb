f58040ff1b226770a8fb7d2608acc3b331babb7f
Eddie Leeper <eddie@notbinary.co.uk>
2020-03-31 10:59:57 +0100

    Tasks view and edit - fix message.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,7 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to task_path(@task), notice: 'Contact was successfully updated.'
+      redirect_to task_path(@task), notice: 'Task was successfully updated.'
     else
       @users = User.all
       render :edit
