65b2a845fb5a213ccccd803e11b477bfadd31e6b
Eddie Leeper <eddie@notbinary.co.uk>
2020-03-31 10:49:46 +0100

    Tasks view and edit - initial version.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
+
   def index
     @tasks = Task.includes(:contact, :user).page(params[:page])
   end
@@ -19,7 +21,24 @@ class TasksController < ApplicationController
     redirect_to controller: :contacts, action: :show_tasks, id: @contact.id
   end
 
+  def edit
+    @users = User.all
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: 'Contact was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
+    def set_task
+      @task = Task.find(params[:id])
+      @contact = @task.contact
+    end
+
     def task_params
       params.require(:task).permit(:name, :due_by, :user_id, :category)
     end
