b96f3448b4a3c63109a4f937d454f81c3359686e
jamie <jbuckley760@gmail.com>
2020-04-01 16:34:08 +0100

    Remove extra if check

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -16,7 +16,7 @@ class NeedsController < ApplicationController
     if params[:order].present? && params[:order_dir].present?
       sort_column = Need.column_names.include?(params[:order]) ? params[:order] : 'created_at'
       sort_order = %w(asc desc).include?(params[:order_dir].downcase) ? params[:order_dir] : 'desc'
-      @needs = @needs.order("#{sort_column} #{sort_order}") if params[:order].present? && params[:order_dir].present?
+      @needs = @needs.order("#{sort_column} #{sort_order}")
     else
       @needs = @needs.order(created_at: :desc)
     end
