85e616e655002ad7189775372784d1a97a063320
jamie <jbuckley760@gmail.com>
2020-04-01 16:02:57 +0100

    Remove 'my_needs' controller

--- app/controllers/my_needs_controller.rb
+++
@@ -1,7 +0,0 @@
-class MyNeedsController < ApplicationController
-  include ParamsConcern
-  def index
-    @needs = current_user.needs.includes(:contact, :user).page(params[:page])
-    @users = [current_user]
-  end
-end
