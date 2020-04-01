adc6ec2960599d5246cacdb44225c23c85e03efe
Riccardo Noviello <riccardo.noviello.contractor@dvla.gov.uk>
2020-04-01 11:58:48 +0100

    add filters to top bar and refactor filter logic

--- app/controllers/my_needs_controller.rb
+++ app/controllers/my_needs_controller.rb
@@ -1,4 +1,5 @@
 class MyNeedsController < ApplicationController
+  include ParamsConcern
   def index
     @needs = current_user.needs.includes(:contact, :user).page(params[:page])
     @users = [current_user]
