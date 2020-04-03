f8ff9b49ac686fbe884ff2290eb5faf5174ae27c
Richard Brooke <ragnarcom@googlemail.com>
2020-04-03 14:34:47 +0100

    removed debugging code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-puts "RAILS_ENV is %s" % ENV['RAILS_ENV']
 require File.expand_path('../config/environment', __dir__)
-puts "Rails.env is %s" % Rails.env
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
