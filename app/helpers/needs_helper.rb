2747b8538c463034f9da979e2b5f6fc1224fccb6
Richard Brooke <ragnarcom@googlemail.com>
2020-04-03 10:55:51 +0100

    added Phone Triage category

--- app/helpers/needs_helper.rb
+++ app/helpers/needs_helper.rb
@@ -16,7 +16,7 @@ module NeedsHelper
   end
 
   def needs
-    ['Groceries and cooked meals', 'Physical and mental wellbeing', 'Financial support',
+    ['Phone Triage','Groceries and cooked meals', 'Physical and mental wellbeing', 'Financial support',
      'Staying Social', 'Prescription pickups', 'Book drops and entertainment', 'Dog walking']
   end
 end
