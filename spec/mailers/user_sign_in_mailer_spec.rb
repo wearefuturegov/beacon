a686b858ef2ddd0b22db2b7adf2a512e8dcc97eb
laptop <riccardonoviello@futuregov.com>
2020-04-06 17:46:48 +0100

    fix spec

--- spec/mailers/user_sign_in_mailer_spec.rb
+++ spec/mailers/user_sign_in_mailer_spec.rb
@@ -1,20 +1,27 @@
 require "rails_helper"
 
 RSpec.describe UserSignInMailer, type: :mailer do
-  describe 'send invite email' do
-    let(:user) { double User, email: 'test@emailaddress.com' }
-    let(:mail) { described_class.send_invite_email(user).deliver }
-
-    # it 'renders the subject' do
-    #   expect(mail.subject).to eq("You've been invited")
-    # end
-
-    it 'sends the email to the user' do
-      expect(mail.to).to eq(["test@emailaddress.com"])
+  before(:each) do
+    routes.draw do
+      # mock get 'root_url'
+      get '/' => 'test#index'
     end
+  
+    describe 'send invite email' do
+      let(:user) { double User, email: 'test@emailaddress.com' }
+      let(:mail) { described_class.send_invite_email(user).deliver }
 
-    it 'is sent from the applications noreply address' do
-      expect(mail.from).to eq(["help@beacon.support"])
+      it 'renders the subject' do
+        expect(mail.subject).to eq("You've been invited")
+      end
+
+      it 'sends the email to the user' do
+        expect(mail.to).to eq(["test@emailaddress.com"])
+      end
+
+      it 'is sent from the applications noreply address' do
+        expect(mail.from).to eq(["help@beacon.support"])
+      end
     end
   end
 end
