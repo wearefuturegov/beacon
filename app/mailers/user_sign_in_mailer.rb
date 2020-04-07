74618b1e11fdaa07e8fe90856f751b8d14a84f63
laptop <riccardonoviello@futuregov.com>
2020-04-07 08:01:22 +0100

    amend rspec

--- app/mailers/user_sign_in_mailer.rb
+++ app/mailers/user_sign_in_mailer.rb
@@ -2,10 +2,11 @@ class UserSignInMailer < GovukNotifyRails::Mailer
   
   default :from => Rails.configuration.councils[ENV['COUNCIL'] || :demo][:default_from_address]
 
-  def send_invite_email(user)
+  def send_invite_email(user, url)
+    
     set_template(Rails.configuration.councils[ENV['COUNCIL'] || :demo][:invite_template])
     set_personalisation(
-      beacon_url: root_url
+      beacon_url: url
     )
     mail(:to => user.email)
   end
--- spec/mailers/user_sign_in_mailer_spec.rb
+++ spec/mailers/user_sign_in_mailer_spec.rb
@@ -1,27 +1,31 @@
 require "rails_helper"
 
 RSpec.describe UserSignInMailer, type: :mailer do
-  before(:each) do
-    routes.draw do
-      # mock get 'root_url'
-      get '/' => 'test#index'
+  describe 'send invite email' do
+
+    let(:template) { '31d2b530-8db4-44ce-9bfa-34b01519335f' }
+    let(:user) { double User, email: 'test@emailaddress.com' }
+    let(:mail) { described_class.send_invite_email(user, 'http://localhost:3000/') }
+    let(:personalisation) { { beacon_url: 'http://localhost:3000/' } }
+
+    it 'sets the recipient' do
+      expect(mail.to).to eq(['test@emailaddress.com'])
     end
-  
-    describe 'send invite email' do
-      let(:user) { double User, email: 'test@emailaddress.com' }
-      let(:mail) { described_class.send_invite_email(user).deliver }
+    
+    it 'sets the subject' do
+      expect(mail.body).to match("This is a GOV.UK Notify email with template #{template}")
+    end
 
-      it 'renders the subject' do
-        expect(mail.subject).to eq("You've been invited")
-      end
+    it 'sets the template' do
+      expect(mail.govuk_notify_template).to eq(template)
+    end
 
-      it 'sends the email to the user' do
-        expect(mail.to).to eq(["test@emailaddress.com"])
-      end
+    it 'does not set the reference' do
+      expect(mail.govuk_notify_reference).to be_nil
+    end
 
-      it 'is sent from the applications noreply address' do
-        expect(mail.from).to eq(["help@beacon.support"])
-      end
+    it 'sets the personalisation' do
+      expect(mail.govuk_notify_personalisation.keys).to eq([:beacon_url])
     end
   end
 end
