85d54f4fbaf3f4a86922b2377bde885429775579
jamie <jbuckley760@gmail.com>
2020-04-08 15:18:56 +0100

    Front end styling/js for navbar functionality

---
+++ app/javascript/packs/navbar.js
@@ -0,0 +1,20 @@
+const sections = Object.assign({}, ...Array.from(document.querySelectorAll("a.anchor-point"))
+    .map(section => ({ [section.id]: section.offsetTop })));
+
+// todo: this should use a proper library, e.g. scrollspy
+const updateHighlightedAnchor = () => {
+    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollSections = Object.entries(sections).filter(e => scrollPosition > e[1] - 50);
+    const scrollSection = scrollSections[scrollSections.length - 1];
+    if (scrollSection != null) {
+        let currentlyActive = document.querySelector('.sidenav .active');
+        if (currentlyActive != null) {
+            currentlyActive.classList.remove('active');
+        }
+        document.querySelector('.sidenav a[href*=' + scrollSection[0] + ']').setAttribute('class', 'active');
+    }
+};
+
+window.onscroll = () => {
+    updateHighlightedAnchor();
+};
