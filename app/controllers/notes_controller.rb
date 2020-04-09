7039a366027a892ee86dde40964cd4c3658ec930
Richard Brooke <ragnarcom@googlemail.com>
2020-04-09 15:07:25 +0100

    Map Notes categories to Need categories and edit and show them on needs/show

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,13 +3,13 @@
 class NotesController < ApplicationController
   def create
     @need = Need.find(params[:need_id])
-    @note = @need.notes.create(note_params.merge(user: current_user))
+    @note = @need.notes.create!(note_params.merge(user: current_user))
     redirect_to need_path(@need)
   end
 
   private
 
   def note_params
-    params.require(:note).permit(:body)
+    params.require(:note).permit(:body, :category)
   end
 end
