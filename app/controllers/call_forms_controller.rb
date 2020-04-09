e1514e4669aca6e1dde7984ab6c1169da0eb6958
jamie <jbuckley760@gmail.com>
2020-04-09 10:16:40 +0100

    Save contact and need information when submitting form

--- app/controllers/call_forms_controller.rb
+++ app/controllers/call_forms_controller.rb
@@ -6,16 +6,32 @@ class CallFormsController < ApplicationController
   before_action :set_contact, only: %i[new create]
 
   def new
-    @call_form = ContactCallForm.new()
+    @call_form = ContactCallForm.new
     @call_form.contact = @contact
   end
 
   def create
-    Rails.logger.debug params.inspect
+    if @contact.update(contact_params)
+      NeedsCreator.create_needs(@contact, call_form_params['needs_list'], call_form_params['other_need'])
+      redirect_to contact_path(@contact), notice: 'Contact was successfully updated.'
+    else
+      render :new
+    end
   end
 
   private
-    def set_contact
-      @contact = Contact.find(params[:contact_id])
-    end
+  def set_contact
+    @contact = Contact.find(params[:contact_id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :middle_names, :surname, :address, :postcode, :email, :telephone,
+                                    :mobile, :additional_info, :is_vulnerable, :count_people_in_house, :any_children_below_15,
+                                    :delivery_details, :any_dietary_requirements, :dietary_details,
+                                    :cooking_facilities, :eligible_for_free_prescriptions)
+  end
+
+  def call_form_params
+    params.require(:contact_call_form).permit!
+  end
 end
