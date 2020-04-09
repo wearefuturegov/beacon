e3a40cb6bb71ff06050c3a727e0bf8c02cf55132
Richard Brooke <ragnarcom@googlemail.com>
2020-04-09 17:10:08 +0100

    rubocop auto fixes

--- spec/helpers/needs_helper_spec.rb
+++ spec/helpers/needs_helper_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe NeedsHelper  do
+RSpec.describe NeedsHelper do
   describe '#needs' do
     it 'returns a list of needs' do
       expect(helper.needs.count).to be > 0
@@ -11,7 +11,7 @@ RSpec.describe NeedsHelper  do
 
   describe '#note_category_display' do
     it 'returns appropriate display values when given a need category and a note category' do
-      expect(helper.note_category_display('phone triage','phone_success')).to be 'Successful Phone call'
+      expect(helper.note_category_display('phone triage', 'phone_success')).to be 'Successful Phone call'
     end
   end
 end
