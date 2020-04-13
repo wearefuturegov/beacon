8f6fc44eeae85c0ff41faa9137ce1cd336d36f60
timwis <tim@timwis.com>
2020-04-13 16:07:59 +0100

    Allow phone_import note categories but don't let users create them

--- app/models/note.rb
+++ app/models/note.rb
@@ -7,7 +7,12 @@ class Note < ApplicationRecord
   enum category: { 'Note': 'general',
                    'Successful Call': 'phone_success',
                    'Left Message': 'phone_message',
-                   'Failed Call': 'phone_failure' }
+                   'Failed Call': 'phone_failure',
+                   'Imported Call Log': 'phone_import' }
 
   validates :body, presence: true
+
+  def self.categories_without_phone_import
+    categories.except('Imported Call Log'.to_sym)
+  end
 end
