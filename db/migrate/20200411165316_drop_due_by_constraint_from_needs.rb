76aa6d01d63704f56ff5745784d5d81b89ab487f
timwis <tim@timwis.com>
2020-04-13 15:35:57 +0100

    Fix migration that drops due_by constraint from needs

--- db/migrate/20200411165316_drop_due_by_constraint_from_needs.rb
+++ db/migrate/20200411165316_drop_due_by_constraint_from_needs.rb
@@ -2,7 +2,7 @@ class DropDueByConstraintFromNeeds < ActiveRecord::Migration[6.0]
   def change
     # Reverses a hard-coded constraint from 20200408110351_add_category_to_notes
     safety_assured do
-      execute 'ALTER TABLE "needs" DROP CONSTRAINT "tasks_due_by_null"'
+      execute 'ALTER TABLE "needs" DROP CONSTRAINT IF EXISTS "tasks_due_by_null"'
     end
   end
 end
