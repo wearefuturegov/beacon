ce3b6a8fec7c028d03e8678276f569f1d853f3c2
jamie <jbuckley760@gmail.com>
2020-04-14 17:22:50 +0100

    Add acceptance tests to cover reassigning and completing a need

--- features/edit_need.feature
+++ features/edit_need.feature
@@ -8,22 +8,20 @@ Feature: Add needs
 
   Scenario: Assign myself a need
     Given a resident with a need exists
-    And I am viewing the need
     When I assign the need to me
     Then I see the need in the 'assigned to me' page
     And I see the updated need details in the contact's 'needs' list
-#
-#  Scenario: Reassign a need from myself to another user
-#    Given a resident with a need exists
-#    And I am viewing the need
-#    And the need is assigned to me
-#    When I assign the need to another user
-#    Then I no longer see the need in the 'assigned to me' page
-#    And I see the updated need details in the contact's needs list
-#
-#  Scenario:
-#    Given a resident with a need exists
-#    And I am viewing the need
-#    And the need has status 'to do'
-#    When I change the need status to 'complete'
-#    Then I see the updated need details in the contact's needs list
+
+  Scenario: Reassign a need from myself to another user
+    Given a resident with a need exists
+    And I have assigned the need to me
+    When I assign the need to another user
+    Then I no longer see the need in the 'assigned to me' page
+    And I see the updated need details in the contact's 'needs' list
+
+  Scenario:
+    Given a resident with a need exists
+    And I have assigned the need to me
+    And the need has status 'to do'
+    When I change the need status to 'complete'
+    Then I see the updated need details in the contact's 'completed' list
--- features/step_definitions/edit_need_steps.rb
+++ features/step_definitions/edit_need_steps.rb
@@ -3,28 +3,26 @@ Given('a resident with a need exists') do
   @need = Need.create!(contact_id: @contact.id, name: 'Phone Triage', category: 'Phone Triage')
 end
 
-When('I am viewing the need') do
+When('I (assign)/(have assigned) the need to me') do
   visit "needs/#{@need.id}"
-end
-
-When('I assign the need to me') do
   page.select 'admin@test.com', from: 'need_user_id'
+  @expected_assignee = 'admin@test.com'
 end
 
 When('I assign the need to another user') do
-  pending
+  @another_user = User.create!(email: 'other_user@email.com', invited: Date.today)
+  visit "needs/#{@need.id}"
+  page.select 'other_user@email.com', from: 'need_user_id'
+  @expected_assignee = 'other_user@email.com'
 end
 
 When("I change the need status to 'complete'") do
-  pending
-end
-
-And('the need is assigned to me') do
-  pending
+  visit "needs/#{@need.id}"
+  page.select 'Complete', from: 'need_status'
 end
 
 And("the need has status 'to do'") do
-  pending
+  # need is incomplete by default
 end
 
 Then("I see the need in the 'assigned to me' page") do
@@ -34,22 +32,24 @@ Then("I see the need in the 'assigned to me' page") do
   expect(assignee_column).to have_content 'admin@test.com'
 end
 
+Then("I no longer see the need in the 'assigned to me' page") do
+  visit '/?user_id=1'
+  content_panel = find('div.panel')
+  expect(content_panel).to have_content 'No matches'
+end
+
 And("I see the updated need details in the contact's {string} list") do |area|
   visit "/contacts/#{@contact.id}"
   need_row = get_area_panel(area).find('tbody tr')
   assignee_column = need_row.find('td:nth-child(3)')
-  expect(assignee_column).to have_content 'admin@test.com'
-end
-
-Then("I no longer see the need in the 'assigned to me' page") do
-  pending
+  expect(assignee_column).to have_content @expected_assignee
 end
 
 def get_area_panel(area)
   case area
   when 'needs'
     panel_selector = '.with-left-sidebar__right .panel.panel--unpadded:nth-of-type(1)'
-  when 'to do'
+  when 'completed'
     panel_selector = '.with-left-sidebar__right .panel.panel--unpadded:nth-of-type(2)'
   else
     raise "Cannot look for non-existent panel #{area}"
