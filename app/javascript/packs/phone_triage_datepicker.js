e7fefcea48b401f7dc5a122cce396cb196edd148
Jamie Buckley <jbuckley760@gmail.com>
2020-04-15 13:54:14 +0100

    Set default date for datepicker

--- app/javascript/packs/phone_triage_datepicker.js
+++ app/javascript/packs/phone_triage_datepicker.js
@@ -4,4 +4,5 @@ const dateElement = document.getElementById("contact_needs_needs_list[0][start_o
 flatpickr(dateElement, {
     minDate: "today",
     dateFormat: "d/m/Y",
+    defaultDate: new Date().fp_incr(6)
 });
