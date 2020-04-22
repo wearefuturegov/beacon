85556f38b87c2cbc0dcafead5555364609698f25
riccardo <riccardonoviello@futuregov.com>
2020-04-22 16:05:59 +0100

    add share data consent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_121439) do
+ActiveRecord::Schema.define(version: 2020_04_22_145507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,9 @@ ActiveRecord::Schema.define(version: 2020_04_22_121439) do
     t.string "channel"
     t.boolean "enquiry_referral", default: false
     t.text "enquiry_message"
+    t.boolean "no_calls_flag", default: false
+    t.boolean "deceased_flag", default: false
+    t.boolean "share_data_flag"
     t.index ["contact_list_id"], name: "index_contacts_on_contact_list_id"
   end
 
