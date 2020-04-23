d41192bba018a812af85c62b7a44e8b94a19f41e
timwis <tim@timwis.com>
2020-04-23 18:52:07 +0100

    Remove references to ActsAsTaggableOn class in old migrations These migrations were failing in the CI because the library has been removed from Gemfile. This commit replaces references with the actual table name. But, in one file's case, the migration was also calling class methods. That really shouldn't happen in a migration to begin with, but there was no way to replace these so I simply removed those lines.

--- db/migrate/20200328094817_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
+++ db/migrate/20200328094817_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
@@ -1,15 +1,10 @@
 # This migration comes from acts_as_taggable_on_engine (originally 3)
 class AddTaggingsCounterCacheToTags < ActiveRecord::Migration[6.0]
   def self.up
-    add_column ActsAsTaggableOn.tags_table, :taggings_count, :integer, default: 0
-
-    ActsAsTaggableOn::Tag.reset_column_information
-    ActsAsTaggableOn::Tag.find_each do |tag|
-      ActsAsTaggableOn::Tag.reset_counters(tag.id, ActsAsTaggableOn.taggings_table)
-    end
+    add_column :tags, :taggings_count, :integer, default: 0
   end
 
   def self.down
-    remove_column ActsAsTaggableOn.tags_table, :taggings_count
+    remove_column :tags, :taggings_count
   end
 end
--- db/migrate/20200328094818_add_missing_taggable_index.acts_as_taggable_on_engine.rb
+++ db/migrate/20200328094818_add_missing_taggable_index.acts_as_taggable_on_engine.rb
@@ -3,14 +3,14 @@ class AddMissingTaggableIndex < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def self.up
-    add_index ActsAsTaggableOn.taggings_table,
+    add_index :taggings,
               [:taggable_id, :taggable_type, :context],
               name: 'taggings_taggable_context_idx',
               algorithm: :concurrently
   end
 
   def self.down
-    remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_taggable_context_idx'
+    remove_index :taggings, name: 'taggings_taggable_context_idx'
   end
 
 end
