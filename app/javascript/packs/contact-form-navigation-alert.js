3e11c0d911c799e9f8e499a7abdbc37b01457b24
Slav Petkov <slav.petkov@saorsa.bg>
2020-04-28 16:05:22 +0300

    Browser displays warning dialog if user tries to navigate away from edit/new contact form with unsaved changes

---
+++ app/javascript/packs/contact-form-navigation-alert.js
@@ -0,0 +1,23 @@
+$(() => {
+   $("body").attr("data-turbolinks", false);
+
+   const formElement = $("form");
+   const originalState = formElement.serialize();
+   formElement.on("submit", onSubmit)
+
+   function shouldNavigateDirectly() {
+      const currentState = $("form").serialize();
+
+      $("*").blur();
+      return currentState === originalState ? undefined : false;
+   };
+
+   function onSubmit() {
+      $(window).off("beforeunload", shouldNavigateDirectly);
+   }
+
+   $(window).on("beforeunload", shouldNavigateDirectly);
+})
+
+
+
