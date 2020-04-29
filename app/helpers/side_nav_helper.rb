d8186186d516e90757d555e053ce2a8b07c60221
Eddie Leeper (NB) <eddie@notbinary.co.uk>
2020-04-29 16:22:58 +0100

    420 interim checkin before merge with master

---
+++ app/helpers/side_nav_helper.rb
@@ -0,0 +1,29 @@
+module SideNavHelper
+  def display_support_actions?
+    mdt_council_or_food_role? ? false : true
+  end
+
+  def current_role_id
+    current_user.role.id
+  end
+
+  private
+
+  def mdt_council_or_food_role?
+    council_service_roles.any? { |role| current_user.in_role_name?(role.tag) } ||
+      user_is_in_mdt_role? ||
+      user_is_in_food_role?
+  end
+
+  def council_service_roles
+    Role.where("role like 'council_service%'")
+  end
+
+  def user_is_in_mdt_role?
+    current_user.in_role_name?('mdt')
+  end
+
+  def user_is_in_food_role?
+    current_user.in_role_name?('food_delivery_manager')
+  end
+end
--- app/models/need.rb
+++ app/models/need.rb
@@ -55,6 +55,11 @@ class Need < ApplicationRecord
     end
   }
 
+  scope :filter_by_team, lambda { |role_id|
+    where(assigned_to: User.with_role(role_id))
+    # where(role_id: role_id).or(assigned_to: User.with_role(role_id))
+  }
+
   scope :filter_by_status, ->(status) { where(status: status) }
 
   scope :filter_by_is_urgent, lambda { |is_urgent|
