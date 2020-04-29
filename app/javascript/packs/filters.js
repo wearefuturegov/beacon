8a61c958944a3fd901bd0ce88ac513fc29addeab
Slav Petkov <slav.petkov@saorsa.bg>
2020-04-30 01:43:15 +0300

    Wrapping filters and export on narrower screens

--- app/javascript/packs/filters.js
+++ app/javascript/packs/filters.js
@@ -1,17 +1,22 @@
-const filters = document.querySelector(".filters")
+const filters = document.querySelector(".filters");
+const exportButton = document.querySelector("#btnExport");
 
-let control = filters.querySelector(".filters__controls")
-let contents = filters.querySelector(".filters__content")
+let control = filters.querySelector(".filters__controls");
+let contents = filters.querySelector(".filters__content");
 
 control && control.addEventListener("click", e  => {
     if(contents.hasAttribute("hidden")){
-        control.setAttribute("aria-expanded", "true")
+        control.setAttribute("aria-expanded", "true");
         control.innerHTML = "Close filters"
         contents.removeAttribute("hidden")
+        filters.setAttribute("data-open", "true");
+        exportButton.setAttribute("data-open", "true");
     } else {
         control.setAttribute("aria-expanded", "false")
-        control.innerHTML = "Filters"
-        contents.setAttribute("hidden", "true")
+        control.innerHTML = "Filters";
+        contents.setAttribute("hidden", "true");
+        filters.removeAttribute("data-open");
+        exportButton.removeAttribute("data-open");
     }
 })
 
