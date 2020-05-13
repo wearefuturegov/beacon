182b2d0a86abc2ec960e594ece67e1bfa0fdd88f
riccardo <riccardonoviello@futuregov.com>
2020-05-13 20:39:23 +0100

    fix merge issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 require("select2/dist/css/select2");
 require("select2");
-
 
 import Popup from "../packs/popup"
 window.Popup = Popup;
