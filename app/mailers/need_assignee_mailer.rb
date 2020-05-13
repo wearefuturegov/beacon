92843d7be0f0d33138b424de553e13671004f9ae
jamie <jbuckley760@gmail.com>
2020-05-13 13:51:46 +0100

    Simplify bulk email code

--- app/mailers/need_assignee_mailer.rb
+++ app/mailers/need_assignee_mailer.rb
@@ -2,7 +2,7 @@ class NeedAssigneeMailer < GovukNotifyRails::Mailer
   default from: Rails.configuration.councils[ENV['COUNCIL'] || :demo][:default_from_address]
 
   def send_user_assigned_need_email(email, url)
-    set_template(Rails.configuration.councils[ENV['COUNCIL'] || :demo][:need_assigned_template])
+    set_template(Rails.configuration.councils[ENV['COUNCIL'] || :demo][:need_assigned_user_template])
     set_personalisation(
       beacon_url: url
     )
--- app/services/needs_assignee_notifier.rb
+++ app/services/needs_assignee_notifier.rb
@@ -1,16 +1,8 @@
 class NeedsAssigneeNotifier
   def self.notify_new_assignee(need)
     return unless need.saved_change_to_user_id? || need.saved_change_to_role_id?
-
-    if need.user_id?
-      user = User.find(need.user_id)
-      NeedAssigneeMailer.send_user_assigned_need_email(user.email, Rails.application.routes.url_helpers.need_url(need)).deliver
-    elsif need.role_id?
-      role_members = User.joins(:roles).where(roles: { id: need.role_id }).select(:email)
-      role_members.each do |role_member|
-        NeedAssigneeMailer.send_role_assigned_need_email(role_member.email, need.role.name, Rails.application.routes.url_helpers.need_url(need)).deliver
-      end
-    end
+    need_url = Rails.application.routes.url_helpers.need_url(need)
+    send_need_email(need, need_url)
   end
 
   def self.bulk_notify_new_assignee(needs)
@@ -19,13 +11,17 @@ class NeedsAssigneeNotifier
 
     change_links = changed_needs.map { |need| "* #{Rails.application.routes.url_helpers.need_url(need)}" }.join("\n")
     need = changed_needs.first
+    send_need_email(need, change_links)
+  end
+
+  def self.send_need_email(need, email_links)
     if need.user_id?
       user = User.find(need.user_id)
-      NeedAssigneeMailer.send_user_assigned_need_email(user.email, change_links).deliver
+      NeedAssigneeMailer.send_user_assigned_need_email(user.email, email_links).deliver
     elsif need.role_id?
       role_members = User.joins(:roles).where(roles: { id: need.role_id }).select(:email)
       role_members.each do |role_member|
-        NeedAssigneeMailer.send_role_assigned_need_email(role_member.email, need.role.name, change_links).deliver
+        NeedAssigneeMailer.send_role_assigned_need_email(role_member.email, need.role.name, email_links).deliver
       end
     end
   end
