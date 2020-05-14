d19f91999ddc3d5cbf06c3e1c2fa745eb2fd558b
riccardo <riccardonoviello@futuregov.com>
2020-05-14 17:30:57 +0100

    save work in progress

--- app/javascript/packs/accordion.js
+++ app/javascript/packs/accordion.js
@@ -1,15 +1,24 @@
-const accordion = document.querySelector(".accordion")
+function startAccordions(){
+    console.log("accordion started");
+    const accordion = document.querySelector(".accordion")
 
-let controls = Array.prototype.slice.call(accordion.querySelectorAll(".accordion__control"), 0)
-let contents = accordion.querySelectorAll(".accordion__content")
+    let controls = Array.prototype.slice.call(accordion.querySelectorAll(".accordion__control"), 0)
+    let contents = accordion.querySelectorAll(".accordion__content")
 
-controls.forEach((control, i) => control.addEventListener("click", e  => {
-    if(contents[i].hasAttribute("hidden")){
-        control.setAttribute("aria-expanded", "true")
-        contents[i].removeAttribute("hidden")
-    } else {
-        control.setAttribute("aria-expanded", "false")
-        contents[i].setAttribute("hidden", "true")
-    }
-    e.preventDefault()
-}))
+    controls.forEach((control, i) => control.addEventListener("click", e  => {
+        if(contents[i].hasAttribute("hidden")){
+            control.setAttribute("aria-expanded", "true")
+            contents[i].removeAttribute("hidden")
+        } else {
+            control.setAttribute("aria-expanded", "false")
+            contents[i].setAttribute("hidden", "true")
+        }
+        e.preventDefault()
+    }));
+}
+
+$( document ).ready(() => {
+    startAccordions();
+});
+
+$('#contact-side-container').change(() => startAccordions());
--- app/javascript/packs/users_viewing_page.js
+++ app/javascript/packs/users_viewing_page.js
@@ -7,16 +7,17 @@ let interval = null;
 const subscription = consumer.subscriptions.create({ channel: 'ContactChannel', id: contactId }, {
     connected() {
         // Calls `ContactChannel#appear(data)` on the server.
-        interval = setInterval(() => { 
-         this.perform("viewing", { user_email: getUserEmail(), contact_id: contactId });
-        }, 3000);
+      //  interval = setInterval(() => { 
+      //   this.perform("viewing", { user_email: getUserEmail(), contact_id: contactId });
+      //  }, 3000);
         
     },
     received(data) {
         const person = currentPeople.find(p => p.name === data.userEmail);
         if (person == null) currentPeople.push({ name: data.userEmail, lastSeen: Date.now() });
         else person.lastSeen = Date.now();
-        if (data.type === 'VIEW') {
+
+        if (data.type === 'VIEW' || data.type === 'EDIT') {
             updateViewings();
         } else if (data.type === 'CHANGED') {
             currentPeople = [];
@@ -42,16 +43,22 @@ const getUserEmail = () => {
     return document.getElementById('userEmail').textContent;
 }
 
-const updateEdits = (userEmail) => {
+const updateEdits = (userEmail) => {    
     const div = document.getElementById("concurrent-users");
     div.innerHTML = '';
-    div.className = 'outdated';
-    const ul = document.createElement("ul");
-    div.appendChild(ul);
-    ul.textContent = 'This record is out of date';
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode("Contact: " + userEmail + " for clarifications."));
-    ul.appendChild(li);
+
+    if (getUserEmail() != userEmail) {
+        div.className = 'outdated';
+        const ul = document.createElement("ul");
+        div.appendChild(ul);
+        ul.textContent = 'This record has been changed';
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode("Contact: " + userEmail + " for clarifications."));
+        ul.appendChild(li);
+        setTimeout(function() {
+            li.innerHTML = '';
+        }, 5000);
+    }
 }
 
 const updateViewings = () => {
@@ -65,7 +72,7 @@ const updateViewings = () => {
         const ul = document.createElement("ul");
         
         div.appendChild(ul);
-        ul.textContent = 'Somebody else is viewing this page, you may be unable to save changes';
+        ul.textContent = 'Somebody else is viewing this record';
         // add other users looking at this page
         otherPeople.forEach(p => {
                 const li = document.createElement("li");
