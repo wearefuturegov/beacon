76edc3b231b9c388e0fa20d9f0835d1c53236d62
Slav Petkov <slav.petkov@saorsa.bg>
2020-05-19 19:50:12 +0300

    Implemented direct navigation to assessment

--- app/javascript/packs/needs-show.js
+++ app/javascript/packs/needs-show.js
@@ -1,8 +1,13 @@
-$(() => {
-    $(".buttons-container .button-link").click(element => {
+$(document).on("turbolinks:load", () => {
+    const linkButton = $(".buttons-container .button-link");
+    linkButton.click(element => {
         $(".needs-container").addClass("invisible");
         $(".notes-container").addClass("invisible");
         $(".buttons-container").addClass("invisible");
         $(".make-call-container").removeClass("invisible");
     });
+    if (localStorage.getItem("startAssessment")) {
+        localStorage.removeItem("startAssessment")
+        linkButton.click();
+    }
 });
