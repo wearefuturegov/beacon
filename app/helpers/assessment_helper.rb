4e40453a73d510418b1e32d654aaddf9cd5f4297
jamie <jbuckley760@gmail.com>
2020-05-19 14:32:10 +0100

    Add e2e tests for assessment failure

--- app/helpers/assessment_helper.rb
+++ app/helpers/assessment_helper.rb
@@ -3,9 +3,9 @@
 module AssessmentHelper
   def assessment_failure_reasons
     {
-     :incorrect_or_missing => 'Contact details were incorrect or missing',
-     :call_not_answered => 'The call was not answered',
-     :other => 'Other reason'
+      incorrect_or_missing: 'Contact details were incorrect or missing',
+      call_not_answered: 'The call was not answered',
+      other: 'Other reason'
     }
   end
 end
--- app/models/assessment_failure_form.rb
+++ app/models/assessment_failure_form.rb
@@ -9,16 +9,16 @@ class AssessmentFailureForm
   :incorrect_or_missing.to_s
   validates_presence_of :failure_reason
   validates_presence_of :note_description
-  validates_presence_of :left_message, if: Proc.new { |form| form.failure_reason == 'call_not_answered' }
+  validates_presence_of :left_message, if: proc { |form| form.failure_reason == 'call_not_answered' }
 
   def save(current_user)
-    need = Need.find(self.id)
-    if self.failure_reason == 'incorrect_or_missing'
-      note_description_hash = (self.note_description.downcase.include? '#invalid') ? self.note_description : self.note_description + ' #Invalid'
+    need = Need.find(id)
+    if failure_reason == 'incorrect_or_missing'
+      note_description_hash = note_description.downcase.include?('#invalid') ? note_description : note_description + ' #Invalid'
       need.notes.create!(user: current_user, category: 'general', body: note_description_hash)
       need.update(status: 'blocked')
     else
-      need.notes.create!(user: current_user, category: 'phone_failure', body: self.note_description)
+      need.notes.create!(user: current_user, category: 'phone_failure', body: note_description)
     end
   end
 end
