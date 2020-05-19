7be56dbb5a5240056f5ea8138111657b9867f24b
riccardo <riccardonoviello@futuregov.com>
2020-05-19 17:53:41 +0100

    amend rubocop failures

--- spec/services/needs_creator_spec.rb
+++ spec/services/needs_creator_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe NeedsCreator do
     @need = double('Need')
     statuses = { to_do: 'to_do' }
     allow(@need_class).to receive(:statuses).and_return statuses
-    allow(@need_class).to receive(:find_or_initialize_by).with(anything()).and_return @need
+    allow(@need_class).to receive(:find_or_initialize_by).with(anything).and_return @need
 
     @builder = double(ActiveRecord::Associations::CollectionProxy)
     allow(@builder).to receive_messages(build: @builder, save: @builder)
@@ -20,12 +20,12 @@ RSpec.describe NeedsCreator do
   end
 
   def mock_find_and_destroy
-    allow(@need_class).to receive(:where).with(hash_including(:id=>"1")).and_return @need
+    allow(@need_class).to receive(:where).with(hash_including(id: '1')).and_return @need
     allow(@need).to receive(:exists?).and_return true
     allow(@need).to receive(:first).and_return @need
     allow(@need).to receive(:really_destroy!).and_return true
   end
-  
+
   it 'saves each active need' do
     # three needs, first two active
     needs_list = {
@@ -34,25 +34,25 @@ RSpec.describe NeedsCreator do
       '3' => { 'active' => 'false', 'name' => 'category3', 'id' => '1' }
     }
     mock_find_and_destroy
-    expect(@need).to receive(:update).with(anything()).twice
+    expect(@need).to receive(:update).with(anything).twice
     described_class.create_needs(@test_contact, needs_list, nil)
   end
 
   it 'sets a default description if none is provided' do
     needs_list = { '1' => { 'active' => 'true', 'name' => 'category' } }
 
-    allow(@need).to receive(:update).with(hash_including(:name=>"Contact name needs category")).and_return @need
+    allow(@need).to receive(:update).with(hash_including(name: 'Contact name needs category')).and_return @need
     described_class.create_needs(@test_contact, needs_list, nil)
   end
 
   it 'sets the description from the provided description if provided' do
     needs_list = { '1' => { 'active' => 'true', 'name' => 'category', 'description' => 'test description' } }
 
-    allow(@need).to receive(:update).with(hash_including(:name=>"test description")).and_return @need
+    allow(@need).to receive(:update).with(hash_including(name: 'test description')).and_return @need
     described_class.create_needs(@test_contact, needs_list, nil)
   end
 
-  it "destroys if inactive" do
+  it 'destroys if inactive' do
     needs_list = { '1' => { 'active' => 'false', 'name' => 'category', 'id' => '1' } }
     mock_find_and_destroy
     described_class.create_needs(@test_contact, needs_list, 'test other description')
@@ -62,7 +62,7 @@ RSpec.describe NeedsCreator do
     needs_list = { 1 => { 'active' => 'true', 'name' => 'category', 'start_on' => '1/1/2000' } }
     expected_date = DateTime.new(2000, 1, 1)
 
-    allow(@need).to receive(:update).with(hash_including(:start_on=>expected_date)).and_return @need
+    allow(@need).to receive(:update).with(hash_including(start_on: expected_date)).and_return @need
     described_class.create_needs(@test_contact, needs_list, nil)
   end
 
@@ -70,7 +70,7 @@ RSpec.describe NeedsCreator do
     needs_list = { 1 => { 'active' => 'true', 'name' => 'category', 'start_on' => 'an_invalid_date' } }
     expected_date = DateTime.now.beginning_of_day + 6.days
 
-    allow(@need).to receive(:update).with(hash_including(:start_on=>expected_date)).and_return @need
+    allow(@need).to receive(:update).with(hash_including(start_on: expected_date)).and_return @need
     described_class.create_needs(@test_contact, needs_list, nil)
   end
 end
