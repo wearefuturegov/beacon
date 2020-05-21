f106e328e146285651dc8f8df9aa6928b41f5100
Slav Petkov <slav.petkov@saorsa.bg>
2020-05-21 15:02:09 +0300

    Progress wizard showing correct statuses on assessment failure form

--- app/javascript/packs/assessment_failure.js
+++ app/javascript/packs/assessment_failure.js
@@ -1,3 +1,11 @@
+$(document).ready(() => {
+    const progressWizard = new ProgressWizard();
+    progressWizard.markCallComplete();
+    progressWizard.markTriageFailed();
+    progressWizard.markAssignFailed();
+    progressWizard.markCompleteCurrent();
+});
+
 const descriptionPromptLabel = document.querySelector('label[for=assessment_failure_form_note_description]');
 const leftMessageQuestionSection = document.getElementById('left_message_question_section');
 const noteDescriptionSection = document.getElementById('note_description_section');
--- app/javascript/packs/progress-wizard.js
+++ app/javascript/packs/progress-wizard.js
@@ -7,16 +7,36 @@ class ProgressWizard {
         this._markStepCurrent("make-call");
     }
 
-    markTriageCurrent() {
+    markCallComplete() {
+        this._markStepComplete("make-call");
+    }
 
+    markTriageCurrent() {
+        this._markStepCurrent("triage");
+    }
+
+    markTriageComplete() {
+        this._markStepComplete("triage");
+    }
+
+    markTriageFailed() {
+        this._markStepFailed("triage");
     }
 
     markAssignCurrent() {
+        this._markStepCurrent("assign");
+    }
 
+    markAssignComplete() {
+        this._markStepComplete("assign");
+    }
+
+    markAssignFailed() {
+        this._markStepFailed("assign");
     }
 
     markCompleteCurrent() {
-
+        this._markStepCurrent("complete")
     }
 
     _findStep(stepName) {
@@ -30,12 +50,13 @@ class ProgressWizard {
 
     _markStepComplete(stepName) {
         const currentStep = this._findStep(stepName);
-        currentStep.find("span .indicator.completed").removeClass("invisible")
+        currentStep.addClass("completed-step");
+        currentStep.find("span.indicator.completed").removeClass("invisible")
     }
 
     _markStepFailed(stepName) {
         const currentStep = this._findStep(stepName);
-        currentStep.find("span .indicator.failed").removeClass("invisible");
+        currentStep.find("span.indicator.failed").removeClass("invisible");
     }
 }
 
