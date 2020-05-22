563192a8e6f01567746a5b2a3e0471dd4fda8268
riccardo <riccardonoviello@futuregov.com>
2020-05-22 11:23:43 +0100

    fix show hide completed needs and assessments

--- app/javascript/packs/assessments.js
+++ app/javascript/packs/assessments.js
@@ -3,30 +3,6 @@ require("select2");
 const dropdowns = $("#formAssessment .dropdown");
 dropdowns.select2();
 
-const allNeedsStatusComplete = document.querySelectorAll('.need--status_complete')
-const showHideAllNeedsStatusComplete = (display) => {
-  for(let i = 0; i < allNeedsStatusComplete.length; i++) {
-    allNeedsStatusComplete[i].style.display = display
-  }  
-};
-showHideAllNeedsStatusComplete('none')
-
-let showCompletedNeeds = false;
-const toggleDisplay = (show) => show ? 'table-row' : 'none';
-
-const toggleVisibilityCompletedAssessments = document.getElementById('toggle-visibility-completed-assessments')
-toggleVisibilityCompletedAssessments.addEventListener('click', (e) => {
-    showCompletedNeeds = !showCompletedNeeds;
-    const table = toggleVisibilityCompletedAssessments.parentNode.parentNode
-    if(showCompletedNeeds) {
-      table.classList.add("table-nav-hide")
-    } else {
-      table.classList.remove("table-nav-hide")
-    }
-    showHideAllNeedsStatusComplete(toggleDisplay(showCompletedNeeds))
-    e.preventDefault()
-});
-
 document.getElementById('btnAssessmentDropdown').addEventListener('click', (e) => {
     document.getElementById('assessmentDropdownElements').classList.toggle('assessment-dropdown-show');
     window.addEventListener('click', onWindowClick);
