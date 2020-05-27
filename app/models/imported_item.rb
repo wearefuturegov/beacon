3814d1422acc991a0813f03778a72de313f5664b
Georgi Keremidchiev <go8soft@gmail.com>
2020-05-27 18:39:32 +0300

    Generate triage need on imported contact

--- app/models/imported_item.rb
+++ app/models/imported_item.rb
@@ -12,7 +12,12 @@ class ImportedItem < ApplicationRecord
   def import(params)
     contacts = []
     CSV.foreach(params[:file].path) do |r|
-      contacts << Contact.new(first_name: r[0], surname: r[1], imported_item: self)
+      contacts << Contact.new(
+        first_name: r[0],
+        surname: r[1],
+        imported_item: self,
+        needs: [Need.new(category: 'Triage', start_on: Date.tomorrow, name: 'Imported from CSV')]
+      )
     end
 
     Contact.import! contacts, recursive: true, all_or_none: true
