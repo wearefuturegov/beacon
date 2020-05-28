550e2dc2f35b5243603c0221c6e2e6d4792b314c
riccardo <riccardonoviello@futuregov.com>
2020-05-28 09:45:04 +0100

    initial work for needs actions

--- app/services/needs_assignee_notifier.rb
+++ app/services/needs_assignee_notifier.rb
@@ -1,6 +1,6 @@
 class NeedsAssigneeNotifier
   def self.notify_new_assignee(need)
-    return unless need.saved_change_to_user_id? || need.saved_change_to_role_id? || !need.send_email
+    return unless need.saved_change_to_user_id? || need.saved_change_to_role_id? || need.send_email
 
     need_url = Rails.application.routes.url_helpers.need_url(need)
 
@@ -9,10 +9,11 @@ class NeedsAssigneeNotifier
 
   def self.bulk_notify_new_assignee(needs)
     changed_needs = needs.select { |need| need.saved_change_to_user_id? || need.saved_change_to_role_id? }
-    return unless changed_needs.any? || !need.send_email
+    return unless changed_needs.any?
 
     change_links = changed_needs.map { |need| "* #{Rails.application.routes.url_helpers.need_url(need)}" }.join("\n")
     need = changed_needs.first
+    return unless need.send_email
     send_need_email(need, change_links)
   end
 
