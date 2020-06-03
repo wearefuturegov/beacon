dd1da93959b74d9729874be6c12dbb8245d1c522
riccardo <riccardonoviello@futuregov.com>
2020-06-03 12:03:29 +0100

    add healthcheck endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,10 @@ class ApplicationController < ActionController::Base
   before_action :set_paper_trail_whodunnit
 
   STALE_ERROR_MESSAGE = 'Error. Somebody else has changed this record, please refresh.'
+  
+  def healthcheck
+    render plain: 'OK'
+  end
 
   def council_key
     ENV['COUNCIL'] || 'demo'
