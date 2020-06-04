c0d558dac815b834b34cea9df9d8c471e6fb8989
riccardo <riccardonoviello@futuregov.com>
2020-06-04 09:41:17 +0100

    extract the passwordless expiry time

--- config/initializers/passwordless.rb
+++ config/initializers/passwordless.rb
@@ -2,4 +2,5 @@ config = Rails.configuration.councils[ENV['COUNCIL'] || :demo]
 Passwordless.default_from_address =  config[:default_from_address]
 # Passwordless.restrict_token_reuse = true
 Passwordless.expires_at = lambda { 1.day.from_now } # How long until a passwordless session expires.
-Passwordless.timeout_at = lambda { 1.hour.from_now } # How long until a magic link expires.
+expiry_minutes = ENV['LINK_EXPIRY_MINUTES'].to_i || 60
+Passwordless.timeout_at = lambda { expiry_minutes.minutes.from_now } # How long until a magic link expires.
