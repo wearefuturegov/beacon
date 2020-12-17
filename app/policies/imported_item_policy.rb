a9e38907ec1e95d0a98613a2888e67690857c7dd
Georgi Keremidchiev <go8soft@gmail.com>
2020-12-17 18:03:37 +0200

    Allow contact centre manager to view and create imports

--- app/policies/imported_item_policy.rb
+++ app/policies/imported_item_policy.rb
@@ -2,17 +2,17 @@ class ImportedItemPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       @user.assign_role_if_empty
-      raise 'Cannot determine scope' unless @user.in_role_name?('sysadmin')
+      raise 'Cannot determine scope' unless @user.in_role_name?('sysadmin') || @user.in_role_name?('manager')
 
       ImportedItem.all
     end
   end
 
   def index?
-    sysadmin?
+    sysadmin? || admin?
   end
 
   def create?
-    sysadmin?
+    sysadmin? || admin?
   end
 end
