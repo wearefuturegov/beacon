b1817bcc49d2d1a49052ad04841c0f1565ea2ca3
Dominic Freeman <dominic.freeman123@gmail.com>
2021-02-12 11:50:40 +0000

    Add timestamps to audit logs

--- db/audit_logs_db_schema.rb
+++ db/audit_logs_db_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_113538) do
+ActiveRecord::Schema.define(version: 2021_02_12_110008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_02_10_113538) do
   create_table "audit_logs", force: :cascade do |t|
     t.integer "user_id"
     t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
