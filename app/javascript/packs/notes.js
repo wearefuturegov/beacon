d80ca9dbdca668ca7eb375ea75976227fc0b4ff5
Toby Osbourn <toby@tosbourn.com>
2021-05-20 14:06:29 +0100

    Replaces call categories with a searchable dropdown

---
+++ app/javascript/packs/notes.js
@@ -0,0 +1,5 @@
+require("select2");
+
+$(".note_categories").select2({
+  placeholder: "Search",
+});
--- app/models/note.rb
+++ app/models/note.rb
@@ -7,17 +7,19 @@ class Note < ApplicationRecord
   belongs_to :need
   belongs_to :user, optional: true
 
-  enum category: { 'Note': 'general',
-                   'Successful Call': 'phone_success',
-                   'Left Message': 'phone_message',
-                   'Failed Call': 'phone_failure',
-                   'Imported Call Log': 'phone_import' }
+  #  enum category: { 'Note': 'general',
+  #                    'Successful Call': 'phone_success',
+  #                    'Left Message': 'phone_message',
+  #                    'Failed Call': 'phone_failure',
+  #                    'Imported Call Log': 'phone_import' }
 
-  validates :body, presence: true
+  CALL_TITLES = [
+    'Adhering to guidance',
+    'Advice with traveling',
+    'Food banks - signposted',
+    'Food banks - reffered',
+    'Other'
+  ].freeze
 
   scope :filter_need_not_destroyed, -> { joins(:need).where('needs.deleted_at IS NULL') }
-
-  def self.categories_without_phone_import
-    categories.except('Imported Call Log'.to_sym)
-  end
 end
