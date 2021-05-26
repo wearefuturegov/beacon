79909d1a0ac6cca62c441a6cc0f424c5752bec0a
Toby Osbourn <toby@tosbourn.com>
2021-05-26 11:26:26 +0100

    Use consistent date formatting

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -159,7 +159,7 @@ class AssessmentsController < ApplicationController
 
   def log_assessment
     @need = Need.new(assessment_params.merge(contact_id: @contact.id, start_on: Date.today))
-    @need.name = "#{@need.category} call - #{Time.zone.now.strftime('%d/%m/%y')}"
+    @need.name = "#{@need.category} call - #{Time.zone.now.strftime('%d/%m/%Y')}"
 
     notes_permit_params = notes_params.merge(need: @need, category: 'phone_success', user_id: current_user.id)
 
