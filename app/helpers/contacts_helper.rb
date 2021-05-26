23d8c87fb4c55961f62ac733f73beea06616d1e6
Elaine Osbourn <kittysquee@gmail.com>
2021-05-26 10:26:37 +0100

    Titleize names in contact edit view
    References [Trello ticket 165](https://trello.com/c/zT0LciP2/165-small-improvement-update-names-to-always-be-lowercase-with-capitalised-first-letters)

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -8,4 +8,9 @@ module ContactsHelper
   def short_date_formatter(date)
     date&.to_s(:default)
   end
+
+  def titleize_formatter(name)
+    name = name.to_s
+    [name.upcase, name.downcase].include?(name) ? name.titleize : name
+  end
 end
